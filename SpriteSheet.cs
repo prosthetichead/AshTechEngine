ac1a939d5cde254e32d67c7c5f72e97531b14af5
Ashley <prosthetichead@gmail.com>
2021-09-20 11:32:10 +0930

    sprite sheet default set to 0

--- SpriteSheet.cs
+++ SpriteSheet.cs
@@ -15,7 +15,7 @@ namespace AshTechEngine
         /// <summary>
         /// which sprite on the sheet to draw defaults to 0
         /// </summary>
-        public int spriteNumber = 1;
+        public int spriteNumber = 0;
 
         public SpriteSheet(int singleSpriteWidth, int singleSpriteHeight)
         {
@@ -45,31 +45,5 @@ namespace AshTechEngine
         {
             spriteBatch.Draw(texture, new Rectangle((int)position.X, (int)position.Y, singleSpriteWidth, singleSpriteHeight), GetSourceRectangle(spriteNumber), color, 0f, origin, spriteEffect, depth);
         }
-
-        //public void Draw(SpriteBatch spriteBatch, int spriteNumber, Rectangle destinationRectangle, Vector2 origin, Color color, float depth = 0, SpriteEffects spriteEffect = SpriteEffects.None)
-        // {
-        //     spriteBatch.Draw(texture, destinationRectangle: destinationRectangle, sourceRectangle: GetSourceRectangle(spriteNumber), color: color, 0f, origin, spriteEffect, depth);
-        // }
-
-        // public void Draw(SpriteBatch spriteBatch, int spriteNumber, Point position, float depth, Vector2 origin, Color color, SpriteEffects spriteEffect)
-        // {
-        //     if (spriteNumber >= 0)
-        //    {
-        //        spriteBatch.Draw(texture,
-        //                 new Rectangle(position.X, position.Y, singleSpriteWidth, singleSpriteHeight),
-        //                GetSourceRectangle(spriteNumber),
-        //                color, 0f, origin, spriteEffect, depth);
-        //    }
-        //    else
-        //     {
-        //draw error sprite
-        //         spriteBatch.Draw(errorTexture, new Rectangle((int)position.X, (int)position.Y, singleSpriteWidth, singleSpriteHeight), null, color, 0f, origin, spriteEffect, depth);
-        //      }
-
-        //if (Game.debugMode && showDebugInfo)
-        //{
-        //    spriteBatch.DrawString(Fonts.ArmaFive, "DEPTH: " + CalculateDepth(position), new Vector2(position.X + tileWidth + 2, position.Y), Color.Black, 0f, origin, 1f, SpriteEffects.None, 1);
-        //}
-        //}
     }
 }
